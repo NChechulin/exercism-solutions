cd438cced4323698f33decb209bc0a368d84c261
Nikolay Chechulin <nchechulin@protonmail.com>
2020-10-25 14:24:17 +0300

    fixes

--- rust/grains/src/lib.rs
+++ rust/grains/src/lib.rs
@@ -1,7 +1,10 @@
 pub fn square(s: u32) -> u64 {
-    2 << s
+    if s == 0 || s > 64 {
+        panic!("Square must be between 1 and 64")
+    }
+    1u64 << (s - 1)
 }
 
 pub fn total() -> u64 {
-    (0..64).map(|x| square(x)).sum()
+    (0..64).map(|x| square(x+1)).sum()
 }
