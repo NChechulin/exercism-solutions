dd5baf81d3e9ab3b8c81d363d5d4f8a591a4a105
Nikolay Chechulin <nchechulin@protonmail.com>
2020-10-25 11:41:24 +0300

    add VERSES array and mapping N into const or format

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -1,16 +1,19 @@
+const VERSES: [&'static str; 3] = [
+    "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n",
+    "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n",
+    "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
+];
+
 pub fn verse(n: u32) -> String {
-    if n == 2 {
-        return String::from("2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n");
-    } else if n == 1 {
-        return String::from("1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n");
-    } else if n == 0 {
-        return String::from("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n");
-    } else {
-        return format!(
+    match n {
+        0 => VERSES[0].to_string(),
+        1 => VERSES[1].to_string(),
+        2 => VERSES[2].to_string(),
+        _ => format!(
             "{0} bottles of beer on the wall, {0} bottles of beer.\nTake one down and pass it around, {1} bottles of beer on the wall.\n",
             n,
             n - 1
-        );
+        ),
     }
 }
 
