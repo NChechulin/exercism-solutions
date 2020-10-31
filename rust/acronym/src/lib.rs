a703978350a4e62dee0f6d0ec23521f10e7cde6a
Nikolay Chechulin <nchechulin@protonmail.com>
2020-10-31 22:48:43 +0300

    remove empty words (10/11)

--- rust/acronym/src/lib.rs
+++ rust/acronym/src/lib.rs
@@ -2,6 +2,7 @@ pub fn abbreviate(phrase: &str) -> String {
     let words: Vec<&str> = phrase.split(|c: char| c != '\'' && !c.is_alphabetic()).collect();
     let first_letters: String = words
         .iter()
+        .filter(|word| word.len() > 0)
         .map(|word| word.chars().next().unwrap())
         .collect::<String>()
         .to_uppercase();
