1cee19a8499668888af629bcb1bd03f9d0feabc5
Nikolay Chechulin <nchechulin@protonmail.com>
2020-10-31 23:31:09 +0300

    format (10/11)

--- rust/acronym/src/lib.rs
+++ rust/acronym/src/lib.rs
@@ -1,5 +1,7 @@
 pub fn abbreviate(phrase: &str) -> String {
-    let words: Vec<&str> = phrase.split(|c: char| c != '\'' && !c.is_alphabetic()).collect();
+    let words: Vec<&str> = phrase
+        .split(|c: char| c != '\'' && !c.is_alphabetic())
+        .collect();
     let first_letters: String = words
         .iter()
         .filter(|word| word.len() > 0)
