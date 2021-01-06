c987f8f992002eb5d5e48bec5093bf4b77d964e4
Nikolay Chechulin <nchechulin@protonmail.com>
2021-01-06 16:28:51 +0300

    add sieve

---
+++ rust/sieve/src/lib.rs
@@ -0,0 +1,19 @@
+pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
+    let mut sift: Vec<bool> = vec![true; (upper_bound + 2) as usize];
+    let mut ans: Vec<u64> = vec![];
+    // Mark 0 and 1 as non-prime numbers
+    sift[0] = false;
+    sift[1] = false;
+
+    for i in 2..(upper_bound + 1) {
+        if sift[i as usize] {
+            ans.push(i);
+
+            for j in (2 * i..(upper_bound + 1)).step_by(i as usize) {
+                sift[j as usize] = false;
+            }
+        }
+    }
+
+    ans
+}
