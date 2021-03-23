be5059e240a08446bb007d60e8cbebe141723507
Nikolay Chechulin <nchechulin@aol.com>
2021-03-23 23:07:58 +0300

    add rotational cipher

---
+++ rust/rotational-cipher/src/lib.rs
@@ -0,0 +1,22 @@
+pub fn my_modulo(mut x: i8, modulo: i8) -> u8 {
+    x %= &modulo;
+    x += &modulo;
+    x %= &modulo;
+    x as u8
+}
+
+pub fn rotate(input: &str, key: i8) -> String {
+    input
+        .chars()
+        .map(|c| match c.is_alphabetic() {
+            false => c,
+            true => {
+                let first = match c.is_ascii_lowercase() {
+                    true => 'a' as u8,
+                    false => 'A' as u8,
+                };
+                (first + (c as u8 + my_modulo(key, 26) - first) % 26) as char
+            }
+        })
+        .collect()
+}
