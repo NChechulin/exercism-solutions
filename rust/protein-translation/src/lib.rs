894d48fa02f4f59f410e0c5717e729ea2048b85c
Nikolay Chechulin <nchechulin@protonmail.com>
2021-12-02 14:26:21 +0300

    make of_rna functional

--- rust/protein-translation/src/lib.rs
+++ rust/protein-translation/src/lib.rs
@@ -1,5 +1,5 @@
-use std::cmp::min;
 use std::collections::HashMap;
+use std::str::from_utf8;
 
 pub struct CodonsInfo<'a> {
     codon_names: HashMap<&'a str, &'a str>,
@@ -11,22 +11,12 @@ impl<'a> CodonsInfo<'a> {
     }
 
     pub fn of_rna(&self, rna: &str) -> Option<Vec<&'a str>> {
-        let mut result: Vec<&'a str> = vec![];
-
-        for i in (0..rna.len()).step_by(3) {
-            let codon = &rna[i..min(i + 3, rna.len())];
-            match self.name_for(codon) {
-                None => return None,
-                Some(name) => match name {
-                    "stop codon" => return Some(result),
-                    _ => result.push(name),
-                },
-            }
-        }
-        if result.len() > 0 {
-            return Some(result);
-        }
-        None
+        rna.as_bytes()
+            .chunks(3)
+            .map(from_utf8)
+            .map(|sequence| self.name_for(sequence.unwrap()))
+            .take_while(|&codon| codon != Some("stop codon"))
+            .collect()
     }
 }
 
