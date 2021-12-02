f0bc1c02e6cc4ef7b20fed0d4f0020751e550fe2
Nikolay Chechulin <nchechulin@protonmail.com>
2021-12-02 14:15:00 +0300

    add a bad (but working!) version of protein translation

---
+++ rust/protein-translation/src/lib.rs
@@ -0,0 +1,44 @@
+use std::cmp::min;
+use std::collections::HashMap;
+
+pub struct CodonsInfo<'a> {
+    codon_names: HashMap<&'a str, &'a str>,
+}
+
+impl<'a> CodonsInfo<'a> {
+    pub fn name_for(&self, codon: &str) -> Option<&'a str> {
+        match self.codon_names.get(&codon) {
+            Some(name) => Some(name),
+            None => None,
+        }
+    }
+
+    pub fn of_rna(&self, rna: &str) -> Option<Vec<&'a str>> {
+        let mut result: Vec<&'a str> = vec![];
+
+        for i in (0..rna.len()).step_by(3) {
+            let codon = &rna[i..min(i + 3, rna.len())];
+            match self.name_for(codon) {
+                None => return None,
+                Some(name) => match name {
+                    "stop codon" => return Some(result),
+                    _ => result.push(name),
+                },
+            }
+        }
+        if result.len() > 0 {
+            return Some(result);
+        }
+        None
+    }
+}
+
+pub fn parse<'a>(pairs: Vec<(&'a str, &'a str)>) -> CodonsInfo<'a> {
+    let mut codon_names: HashMap<&'a str, &'a str> = HashMap::new();
+
+    for (codon, name) in pairs {
+        codon_names.insert(codon, name);
+    }
+
+    CodonsInfo { codon_names }
+}
